e5b7491b16dcb6ef80a9208ef5ca116040ca2e04
WizyWoo <72544537+WizyWoo@users.noreply.github.com>
2024-02-02 17:21:44 +0100

    auto Commit

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
 
     public GameObject EasyWin, NormalWin, HardWin;
+    public Toggle VsyncToggleObj;
 
     private void Start()
     {
@@ -29,6 +31,37 @@ public class MainMenu : MonoBehaviour
             HardWin.SetActive(true);
 
         }
+
+        int vSync = PlayerPrefs.GetInt("Vsync");
+
+        if(vSync == 0)
+        {
+
+            VsyncToggleObj.isOn = false;
+
+        }
+
+    }
+
+    public void VsyncToggle()
+    {
+
+        if(VsyncToggleObj.isOn)
+        {
+
+            QualitySettings.vSyncCount = 1;
+            PlayerPrefs.SetInt("Vsync", 1);
+
+        }
+        else
+        {
+
+            QualitySettings.vSyncCount = 0;
+            PlayerPrefs.SetInt("Vsync", 0);
+
+        }
+
+        PlayerPrefs.Save();
 
     }
 
