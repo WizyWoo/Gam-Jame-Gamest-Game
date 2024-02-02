ede76c286d82adcfebfcd42f2c0d0492eb684129
WizyWoo <72544537+WizyWoo@users.noreply.github.com>
2024-02-02 14:32:54 +0100

    auto Commit

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,6 +6,32 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
 
+    public GameObject EasyWin, NormalWin, HardWin;
+
+    private void Start()
+    {
+
+        if(PlayerPrefs.HasKey("EasyWin"))
+        {
+
+            EasyWin.SetActive(true);
+
+        }
+        if(PlayerPrefs.HasKey("NormalWin"))
+        {
+
+            NormalWin.SetActive(true);
+
+        }
+        if(PlayerPrefs.HasKey("HardWin"))
+        {
+
+            HardWin.SetActive(true);
+
+        }
+
+    }
+
     public void QuitGame()
     {
 
