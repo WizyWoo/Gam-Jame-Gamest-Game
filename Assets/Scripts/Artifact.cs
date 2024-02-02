e77769450aed6482cafa7041c0f2d184cdf2b392
Nicolay Joahsen <77834534+Nicrus@users.noreply.github.com>
2024-02-02 14:06:35 +0100

    asd

--- Assets/Scripts/Artifact.cs
+++ Assets/Scripts/Artifact.cs
@@ -30,11 +30,10 @@ public class Artifact : MonoBehaviour
             SoundManager.instance.timer = 0;
             SoundManager.instance.PlaySound(3);
             Instantiate(GameObject.Find("Spooky Ghost(Clone)"));
-          //  if (DifficultyController.Instance.Difficulty == 2)
-          //  {
-          //      Instantiate(GameObject.Find("Spooky Ghost(Clone)"));
-//
-          //  }
+           if (DifficultyController.Instance.Difficulty == 2)
+           {
+               Instantiate(GameObject.Find("Spooky Ghost(Clone)"));
+           }
 
 
 
