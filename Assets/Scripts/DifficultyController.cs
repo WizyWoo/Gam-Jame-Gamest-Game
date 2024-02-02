a42a46cf8511fcf4c23afa1fd95ad16a6d85a804
WizyWoo <72544537+WizyWoo@users.noreply.github.com>
2024-02-02 13:06:07 +0100

    auto Commit

---
+++ Assets/Scripts/DifficultyController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DifficultyController : MonoBehaviour
+{
+
+    public static DifficultyController Instance;
+    public int Difficulty;
+
+    private void Awake()
+    {
+
+        if(Instance == null)
+        {
+            
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+
+        }
+        else
+        {
+
+            Destroy(gameObject);
+
+        }
+
+    }
+
+}
